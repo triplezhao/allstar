8ff6955c3464317e8b205fe3019cc446f7f54dea
zhaotianwu <zhaotianwu@3f2ce0da-b4b9-4e92-ac49-c6e80d1e7945>
2015-09-01 09:11:31 +0000

    1.将开源的 首页重写。 2.带tag的list需要优化性能。 每次都删除tag再添加，会有卡顿

--- tuya/android/project/trunk/app/src/main/java/com/cyou/sticker/business/camera/ui/act/MainActivity.java
+++ tuya/android/project/trunk/app/src/main/java/com/cyou/sticker/business/camera/ui/act/MainActivity.java
@@ -1,35 +1,25 @@
 package com.cyou.sticker.business.camera.ui.act;
 
-import android.graphics.BitmapFactory;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 import com.alibaba.fastjson.JSON;
-import com.cyou.sticker.business.camera.common.DataUtils;
-import com.cyou.sticker.business.camera.common.StringUtils;
-import com.cyou.sticker.business.camera.customview.CommonTitleBar;
-import com.cyou.sticker.business.camera.customview.LabelView;
 import com.cyou.sticker.R;
 import com.cyou.sticker.base.BaseActivity;
+import com.cyou.sticker.base.BaseListAdapter;
+import com.cyou.sticker.business.camera.common.DataUtils;
+import com.cyou.sticker.business.camera.common.StringUtils;
 import com.cyou.sticker.business.camera.data.bean.FeedItem;
-import com.cyou.sticker.business.camera.data.bean.TagItem;
+import com.cyou.sticker.business.camera.ui.viewbinder.TagViewBinder;
 import com.cyou.sticker.business.camera.util.AppConstants;
 import com.cyou.sticker.business.camera.util.CameraManager;
-import com.melnykov.fab.FloatingActionButton;
+import com.cyou.sticker.databinding.ActivityMainCameraBinding;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.ButterKnife;
-import butterknife.InjectView;
 import de.greenrobot.event.EventBus;
 
 /**
@@ -40,35 +30,57 @@ import de.greenrobot.event.EventBus;
  */
 public class MainActivity extends BaseActivity {
 
-    @InjectView(R.id.fab)
-    FloatingActionButton fab;
-    @InjectView(R.id.recycler_view)
-    RecyclerView mRecyclerView;
     private List<FeedItem> feedList;
-    private PictureAdapter mAdapter;
-    private CommonTitleBar titleBar;
+    private BaseListAdapter mAdapter;
 
+    private ActivityMainCameraBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_camera);
-        titleBar = (CommonTitleBar) findViewById(R.id.title_layout);
-        ButterKnife.inject(this);
+        binding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main_camera);
         EventBus.getDefault().register(this);
-        initView();
+        binding.fab.setOnClickListener(this);
 
-        //如果没有照片则打开相机
+
+        mAdapter = new BaseListAdapter(mContext, new TagViewBinder());
+        binding.list.setAdapter(mAdapter);
+
+        binding.swipeContainer.setColorSchemeResources(R.color.google_blue,
+                R.color.google_green,
+                R.color.google_red,
+                R.color.google_yellow);
+
+        binding.swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                sendRequest2RefreshList();
+            }
+        });
+
+        binding.swipeContainer.setEmptyView(binding.emptyView);
+        binding.emptyView.setOnClickListener(this);
+        binding.swipeContainer.showProgress();
+
+
+        sendRequest2RefreshList();
+    }
+
+
+    public void sendRequest2RefreshList() {
         String str = DataUtils.getStringPreferences(getBaseContext(), AppConstants.FEED_INFO);
         if (StringUtils.isNotEmpty(str)) {
-            feedList = JSON.parseArray(str, FeedItem.class);
+            onRefreshSucc(str);
         }
-        if (feedList == null) {
-            CameraManager.getInst().openCamera(MainActivity.this);
-        } else {
-            mAdapter.setList(feedList);
-        }
+    }
 
+    private void onRefreshSucc(String content) {
+        feedList = JSON.parseArray(content, FeedItem.class);
+        binding.swipeContainer.showSucc();
+        mAdapter.setDataList(feedList);
+        mAdapter.notifyDataSetChanged();
+        binding.swipeContainer.setRefreshing(false);
     }
 
 
@@ -78,7 +90,7 @@ public class MainActivity extends BaseActivity {
         }
         feedList.add(0, feedItem);
         DataUtils.setStringPreferences(getBaseContext(), AppConstants.FEED_INFO, JSON.toJSONString(feedList));
-        mAdapter.setList(feedList);
+        mAdapter.setDataList(feedList);
         mAdapter.notifyDataSetChanged();
 
     }
@@ -105,127 +117,17 @@ public class MainActivity extends BaseActivity {
     }
 
     @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+                CameraManager.getInst().openCamera(mContext);
+                break;
+        }
+    }
+
+    @Override
     public void bindEvent() {
 
-    }
-
-    private void initView() {
-        titleBar.hideLeftBtn();
-        titleBar.hideRightBtn();
-
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mAdapter = new PictureAdapter();
-        mRecyclerView.setAdapter(mAdapter);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                CameraManager.getInst().openCamera(mContext);
-            }
-        });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    //照片适配器
-    public class PictureAdapter extends RecyclerView.Adapter<ViewHolder> {
-
-        private List<FeedItem> items = new ArrayList<FeedItem>();
-
-        public void setList(List<FeedItem> list) {
-            if (items.size() > 0) {
-                items.clear();
-            }
-            items.addAll(list);
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_picture, parent, false);
-            return new ViewHolder(v);
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-
-            FeedItem feedItem = items.get(position);
-            holder.picture.setImageBitmap(BitmapFactory.decodeFile(feedItem.getImgPath()));
-            holder.setTagList(feedItem.getTagList());
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return items.size();
-        }
-
-        @Override
-        public void onViewRecycled(ViewHolder holder) {
-            // 将标签移除,避免回收使用时标签重复
-            holder.pictureLayout.removeViews(1, holder.pictureLayout.getChildCount() - 1);
-            super.onViewRecycled(holder);
-        }
-
-        @Override
-        public void onViewAttachedToWindow(final ViewHolder holder) {
-            super.onViewAttachedToWindow(holder);
-            // 这里可能有问题 延迟200毫秒加载是为了等pictureLayout已经在屏幕上显示getWidth才为具体的值
-            holder.pictureLayout.getHandler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    for (TagItem feedImageTag : holder.getTagList()) {
-                        LabelView tagView = new LabelView(MainActivity.this);
-                        tagView.init(feedImageTag);
-                        tagView.draw(holder.pictureLayout,
-                                (int) (feedImageTag.getX() * ((double) holder.pictureLayout.getWidth() / (double) 1242)),
-                                (int) (feedImageTag.getY() * ((double) holder.pictureLayout.getWidth() / (double) 1242)),
-                                feedImageTag.isLeft());
-                        tagView.wave();
-                    }
-                }
-            }, 200);
-        }
-    }
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        @InjectView(R.id.pictureLayout)
-        RelativeLayout pictureLayout;
-        @InjectView(R.id.picture)
-        ImageView picture;
-
-        private List<TagItem> tagList = new ArrayList<>();
-
-        public List<TagItem> getTagList() {
-            return tagList;
-        }
-
-        public void setTagList(List<TagItem> tagList) {
-            if (this.tagList.size() > 0) {
-                this.tagList.clear();
-            }
-            this.tagList.addAll(tagList);
-        }
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-            ButterKnife.inject(this, itemView);
-        }
     }
 
 }
---
+++ tuya/android/project/trunk/app/src/main/java/com/cyou/sticker/business/camera/ui/viewbinder/TagViewBinder.java
@@ -0,0 +1,88 @@
+package com.cyou.sticker.business.camera.ui.viewbinder;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cyou.sticker.R;
+import com.cyou.sticker.base.BaseViewBinder;
+import com.cyou.sticker.base.BaseViewHolder;
+import com.cyou.sticker.business.camera.customview.LabelView;
+import com.cyou.sticker.business.camera.data.bean.FeedItem;
+import com.cyou.sticker.business.camera.data.bean.TagItem;
+import com.cyou.sticker.databinding.ItemPictureBinding;
+
+import java.util.List;
+
+/**
+ * Created by ztw on 2015/7/22.
+ */
+public class TagViewBinder extends BaseViewBinder<TagViewBinder.ViewHolder> {
+
+    public TagViewBinder() {
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(int position, int type, ViewGroup parent) {
+
+        ItemPictureBinding binding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()),
+                R.layout.item_picture,
+                parent,
+                false);
+        ViewHolder holder = new ViewHolder(binding.getRoot());
+        holder.setBinding(binding);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, Object object, int position, int type) {
+        final ItemPictureBinding binding = (ItemPictureBinding) holder.getBinding();
+        FeedItem feedItem = (FeedItem) object;
+        binding.setBean(feedItem);
+
+        final List<TagItem> list = feedItem.getTagList();
+
+        binding.picture.setImageBitmap(BitmapFactory.decodeFile(feedItem.getImgPath()));
+
+//        binding.setList(list);
+        // 将标签移除,避免回收使用时标签重复
+        binding.pictureLayout.removeViews(1, binding.pictureLayout.getChildCount() - 1);
+
+        // 这里可能有问题 延迟200毫秒加载是为了等pictureLayout已经在屏幕上显示getWidth才为具体的值
+//        ImageLoaderUtil.displayImage(bean.getBigCover(), binding.ivPic, R.drawable.def_gray_big);
+        // 这里可能有问题 延迟200毫秒加载是为了等pictureLayout已经在屏幕上显示getWidth才为具体的值
+
+        for (TagItem feedImageTag : list) {
+            LabelView tagView = new LabelView(binding.getRoot().getContext());
+            tagView.init(feedImageTag);
+            tagView.draw(binding.pictureLayout,
+                    (int) (feedImageTag.getX() ),
+                    (int) (feedImageTag.getY() ),
+                    feedImageTag.isLeft());
+            tagView.wave();
+    }
+}
+
+public static class ViewHolder extends BaseViewHolder {
+
+    private ViewDataBinding binding;
+
+    public ViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    public ViewDataBinding getBinding() {
+        return binding;
+    }
+
+    public void setBinding(ViewDataBinding binding) {
+        this.binding = binding;
+    }
+}
+
+}
